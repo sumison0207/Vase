b792294250d30955b8363264264eec1a5cbc9754
Sumire Maruyama <a18.kats@g.chuo-u.ac.jp>
2020-08-14 01:24:43 +0900

    user_show.html.erbを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,13 @@ class UsersController < ApplicationController
   
     def show
       @user = User.find(params[:id]) 
+      if params[:search] == nil
+        @tweets= Tweet.all.order(created_at: "DESC").page(params[:page]).per(6)
+        elsif params[:search] == ''
+        @tweets= Tweet.all.order(created_at: "DESC").page(params[:page]).per(6)
+        else
+        #部分検索
+        @tweets = Tweet.where("body LIKE ? ",'%' + params[:search] + '%').order(created_at: "DESC").page(params[:page]).per(3)
+        end
   end
 end
